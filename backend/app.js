bd933915aaa8da3a480ec441b4c93844f9ffda1d
thanushayan <thanushayan17@gmail.com>
2024-10-07 09:45:08 +0530

    project setup and entry point set

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+
+const express = require ('express')
+require('dotenv').config();
+
+const PORT = process.env.PORT   
+
+const server = () => {
+    console.log("You are listening to port", PORT);
+}
+
+server();
