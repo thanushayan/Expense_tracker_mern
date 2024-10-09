777a209a5390078eabb24cef7f6bb76f90bbe896
thanushayan <thanushayan17@gmail.com>
2024-10-09 10:01:19 +0530

    styled components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import styled from "styled-components";
 import bg from './img/bg.png';
 import {MainLayout} from './styles/Layouts'
+import Orb from "./Components/Orb";
 
  
 function App() {
   return (
     <AppStyled bg={bg} className='App'>
+      <Orb/>
       <MainLayout>
        
       </MainLayout>
---
+++ frontend/src/Components/Navigation/Navigation.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from 'styled-components';
+import avatar from '../../img/avatar.png'
+
+function Navigation() {
+  return (
+    <NavStyled>
+        <div className='user-con'>
+            <img src ={avatar} alt=""/>
+            <div className="text">
+                <h2>Mike</h2>
+                <p>Your Money </p>
+            </div>
+        </div> 
+        <ul className="menu-items">
+            
+        </ul>
+ 
+    </NavStyled>
+  )
+}
+const NavStyled = styled.nav `
+`;
+
+export default Navigation
