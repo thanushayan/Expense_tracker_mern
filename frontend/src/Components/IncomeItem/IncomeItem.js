d3caa2417ccf2ed2dfa153ed2ee8a0221009e034
thanushayan <thanushayan17@gmail.com>
2024-10-11 17:19:57 +0530

    income

---
+++ frontend/src/Components/IncomeItem/IncomeItem.js
@@ -0,0 +1,124 @@
+import React from 'react'
+import styled from 'styled-components'
+import { calender, comment,dollar, trash } from '../../utils/Icon';
+import Button from '../Button/Button';
+
+
+function IncomeItem({
+    id,
+    title,
+    amount,
+    date,
+    category,
+    description,
+    deleteItem,
+    indicatorColor,
+    type
+}) {
+  return (
+    <IncomeItemStyled indicator={indicatorColor}>
+        <div className="icon">
+
+        </div>
+        <div className="content">
+            <h5>{title}</h5>
+            <div className="inner-content">
+                <div className="text">
+                    <p>{dollar} 45</p>
+                    <p>{calender} {date}45</p>
+                    <p>
+                        {comment}
+                        {description}
+                    </p>
+                </div>
+                <div className="btn-con">
+                    <Button
+                    icon={trash}
+                    bPad={'1rem'}
+                    bRad={'50%'}
+                    bg={'var(--primary-color'}
+                    color={'#fff'}
+                    hColor={'var(--color-green)'}
+                    // onClick={()=> handleDelete(id)}
+                    />
+                 </div>
+            </div>
+
+        </div>
+      
+    </IncomeItemStyled>
+  )
+}
+const IncomeItemStyled = styled.div`
+background: #fcf6f9;
+border:2px solid #FFFFFF;
+box-shadow:0px 1px 15px rgba(0,0,0,0.06);
+border-radius:20px;
+padding:1rem;
+margin-bottom:1rem;
+display:flex;
+align-items:center;
+gap:1rem;
+width:100%;
+color:#222260;
+.icon{
+    width:80px;
+    height:80px;
+    border-radius:20px;
+    background: #f5f5f5;
+    display:flex;
+    align-items:center;
+    justify-content: center;
+    border:2px solid #FFFFFF;
+    i{
+        font-size:2.6rem;
+    }
+}
+.content{
+    flex:1;
+    display:flex;
+    flex-direction: column;
+    gap:.2rem;
+    h5{
+        font-size: 1.3rem;
+        padding-left:2rem;
+        position:relative;
+        &::before{
+            content:'';
+            position:absolute;
+            left:0;
+            top:50%;
+            transform:translateY(-50%);
+            width:.8rem;
+            height:.8rem;
+            border-radius:50%;
+            background:${props => props.indicator}
+
+        }
+
+    }
+
+    .inner-content{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        .text{
+            display: flex;
+            align-items: center;
+            gap: 1.5rem;
+            p{
+                display:flex;
+                align-items: center;
+                gap: 0.5rem;
+                color:var(--primary-color);
+                opacity: 0.8;
+
+
+            }
+        }
+
+    }
+}
+`;
+
+export default IncomeItem;
